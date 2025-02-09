9fec8eed922bb148074750eded3dc3cff516b156
Blossomifeoma <Adikankwublossom@gmail.com>
2025-02-09 17:36:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 1000;
 // API route
 app.get("/", (req, res) => {
     res.json({
-        email: "blossomadikankwu@gmail.com.com",
+        email: "blossie668@gmail.com.com",
         current_time: new Date().toISOString(),
         github_url: "https://github.com/blossomifeoma/HNGTask0"
     });
